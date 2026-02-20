bc64b3606456a701e8d8a41b4a9e4ac54f371e9f
Zxectic <133044863+Zxectic@users.noreply.github.com>
2026-02-20 13:32:37 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Khan from './Khan';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Khan />);
